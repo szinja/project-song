87f2571016dab05e12b6b6a067d12722292e0232
szinja <szinja@u.rochester.edu>
2024-05-07 10:21:14 +0200

    Making the case studies responsive and adding text on images.

--- src/components/case-studies/CaseStudies.jsx
+++ src/components/case-studies/CaseStudies.jsx
@@ -24,13 +24,29 @@ const CaseStudies = () => {
   return (
     <section>
       <h4 className="section-title">Case studies</h4>
-      <div className="caseStudy-con">
-        {caseStudy.map((caseStudy, i) => (
-          <div key={i} className="caseStudy-img-con">
-            <img src={caseStudy.logo} alt={caseStudy.name} />
-          </div>
-        ))}
-      </div>
+      <div className="image-gallery">
+            <div className="image-container">
+                <img src={olympian} alt="Olympian" />
+                <div className="text-overlay">
+                    <h2>The Olympian</h2>
+                    <p>The only athlete in the world to do her Olympic routine in 2020. </p>
+                </div>
+            </div>
+            <div className="image-container">
+                <img src={dragon} alt="Dragon" />
+                <div className="text-overlay">
+                    <h2>The Savings Jar</h2>
+                    <p>Grow your savings treasure and grow your dragon.</p>
+                </div>
+            </div>
+            <div className="image-container">
+                <img src={skhokho} alt="Skhokho" />
+                <div className="text-overlay">
+                    <h2>Skhokho seMali</h2>
+                    <p>Helping South Africans become #CashCleva with Skhokho and TymeBank.</p>
+                </div>
+            </div>
+        </div>
     </section>
   );
 };
